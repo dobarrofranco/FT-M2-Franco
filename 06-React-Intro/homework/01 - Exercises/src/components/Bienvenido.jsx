83f550d3c3548628a0720e6794d69145e2bc2b94
dobarrofranco <mr.francodobarro@gmail.com>
2023-08-22 10:17:19 -0300

    adding

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Franco Dobarro";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return(<div>
+    <h1>Título</h1>
+    <h3>{studentName}</h3>
+    <ul>
+      {techSkills.map((element) => {
+        return <li>{element}</li>
+      })}
+    </ul>
+    <Botones alerts={alerts}/>
+  </div>);
 }
 
 // Esto lo exportamos para los tests
---
+++ Diagramas/Notes.js
@@ -0,0 +1,20 @@
+//! Clase React:
+
+//* Conceptos fundamentales:
+
+//? 1: Declarativo: "Que quiero hacer" => React es el que decide como hacerlo. "React quiero modificar este h1" => se encarga de hacerlo react.
+
+//? 2: Flujo de información es unidireccional
+
+//? 3: Componentes reutilizables: componentes que trabajan de forma independiente
+
+
+//! Componenetes de responsabilidad única:
+
+//* - Renderizar una imagen, un texto, un botón.
+
+//* - Renderizar una lista de tarjetas
+
+//* - Etc.
+
+
