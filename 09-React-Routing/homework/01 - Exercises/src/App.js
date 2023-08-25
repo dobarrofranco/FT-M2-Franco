89ca8c56fe6b80ac16a12158c24796400efca208
dobarrofranco <mr.francodobarro@gmail.com>
2023-08-25 16:04:25 -0300

    09

--- 09-React-Routing/homework/01 - Exercises/src/App.js
+++ 09-React-Routing/homework/01 - Exercises/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 /* eslint-disable */
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Shipping from "./components/Shipping/Shipping.jsx";
@@ -7,6 +8,38 @@ import Discounts from "./components/Discounts/Discounts.jsx";
 import CardDetail from "./components/CardDetail/CardDetail.jsx";
 /* eslint-disable */
 
+// Home --> path: "/" element: <Home/>
+// Shipping --> path: "/shipping" element: <Shipping/>
+// Discounts --> path: "/discounts" element: <Discounts/>
+// CardDetail --> path: "/cruises/:id" element: <CardDetail/>
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+
+        <NavBar />
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Home/>}
+        />
+        <Route
+          path="/shipping"
+          element={<Shipping/>}
+        />
+        <Route
+          path="discounts"
+          element={<Discounts/>}
+        />
+        <Route
+          path="cruises/:id"
+          element={<CardDetail/>}
+        />
+
+      </Routes>
+      
+    </div>
+  )
 }
--- 09-React-Routing/homework/01 - Exercises/src/components/NavBar/NavBar.jsx
+++ 09-React-Routing/homework/01 - Exercises/src/components/NavBar/NavBar.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import logoHenry from "../../assets/logo-henry.png";
 import styleNav from "./NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
+
+  function linkActive({ isActive }) {
+    if (isActive) {
+      return styleNav.active;
+    } else {
+      return styleNav.disable;
+    }
+  }
+
   return (
     <div className={styleNav.container}>
       <ul className={styleNav.menu}>
         <li>
-          <img src={logoHenry} alt="logo-henry" />
+          <NavLink to={'/'}>
+            <img src={logoHenry} alt="logo-henry" />
+          </NavLink>
         </li>
         <li>
           <h1>Central de Cruceros</h1>
         </li>
         <div className={styleNav.options}>
           <li>
-            <span>Navieras</span>
+            <NavLink to={'/shipping'} className={linkActive}>
+              <span>Navieras</span>
+            </NavLink>
           </li>
           <li>
-            <span>Promociones</span>
+            <NavLink to={'/discounts'} className={linkActive}>
+              <span>Promociones</span>
+            </NavLink>
           </li>
         </div>
       </ul>
