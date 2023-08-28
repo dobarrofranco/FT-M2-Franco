25e595b0be324d07d33f85cccce0696d2083c0c8
dobarrofranco <mr.francodobarro@gmail.com>
2023-08-28 15:59:57 -0300

    10

--- 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Contact.modules.css'
-import { validaciones } from '../../validates';
 
 // eslint-disable-next-line
 
+const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+export function validate(inputs) {
+    let errors={};
+
+    if (!inputs.name) {
+        errors.name = 'Se requiere un nombre';
+    }
+    
+    if (!regexEmail.test(inputs.email)) {
+        errors.email = 'Debe ser un correo electrónico';
+    }
+
+    if (!inputs.message) {
+        errors.message = 'Se requiere un mensaje';
+    }
+
+    return errors;
+}
+
 export default function Contact () {
-  const [input, setInput] = React.useState({
+  const [input, setInput] = useState({
     name: '',
     email: '',
     message: ''
   })
 
-  const [errors, setErrors] = React.useState({
+  const [errors, setErrors] = useState({
     name: '',
     email: '',
     message: ''
@@ -20,51 +39,57 @@ export default function Contact () {
   const handleChange = (event) => {
     setInput({...input, [event.target.name]: event.target.value});
 
-    setErrors(validaciones({...input, [event.target.name]: event.target.value}));
+    setErrors(validate({...input, [event.target.name]: event.target.value}));
   }
 
   const handleSubmit = (event) => {
     
     event.preventDefault();
 
-    setErrors([]);
+    if (!Object.values(errors).length) {
 
-    if (errors.length === 0) {
+      alert('Datos completos');
      
-      setInput({...input, [event.target.name]: event.target.value});
+      setInput({
+        name: '',
+        email: '',
+        message: ''
+      });
       
-      setErrors(validaciones({...input, [event.target.name]: event.target.value}));
+      setErrors({
+        name: '',
+        email: '',
+        message: ''
+      });
       
-      return alert('Datos completos');
 
-    }
+    }else{
+
+      alert('Debe llenar todos los campos');
     
-    if(errors.length > 2){
-      return alert('Debe llenar todos los campos');
     }
   }
 
   return ( 
-    <div className='formulario'>
+    <div>
       <form onSubmit={handleSubmit}>
-        <p>Nombre</p>
+
+        <label>Nombre:</label>
         <input className={errors.name && 'warning'} type="text" name='name' placeholder='Escribe tu nombre...' value={input.name} onChange={handleChange}/>
         
         <p className='danger'>{errors.name}</p>
-        <br />
 
-        <p>Correo Electrónico</p>
+        <label>Correo Electrónico:</label>
         <input className={errors.email && 'warning'} type="text" name='email' placeholder='Escribe tu email...' value={input.email} onChange={handleChange}/>
         
         <p className='danger'>{errors.email}</p>
-        <br />
 
-        <p>Mensajes</p>
+        <label>Mensaje:</label>
         <textarea className={errors.message && 'warning'} name="message" cols="30" rows="10" placeholder='Escribe tu mensaje...' type="text" value={input.message} onChange={handleChange}></textarea>
         <p className='danger'>{errors.message}</p>
-        <br />
 
         <button type='submit'>Enviar</button>
+
       </form>
 
     </div>
