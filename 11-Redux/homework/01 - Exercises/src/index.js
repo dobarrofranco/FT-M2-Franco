94168ba69fc4eb176f6119b1924800c2a0150fdd
dobarrofranco <mr.francodobarro@gmail.com>
2023-08-28 15:51:16 -0300

    11

--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -3,10 +3,10 @@ const contador = require("./reducer");
 const { incremento, decremento } = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+const store = createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+const valor = document.querySelector('#valor');
 
 // Esta función nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
@@ -14,11 +14,36 @@ var valor;
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
   // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
+  const counter = store.getState().contador;
+  valor.innerHTML = counter;
 }
 
 // Ejecutamos la función 'renderContador':
+renderContador()
 
 // Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
+store.subscribe(renderContador) // Accede a la información del estado global, queda atento a la prop contador
 
 // Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la acción correspondiente:
+
+let botonInc = document.querySelector('#incremento')
+botonInc.addEventListener('click', () => store.dispatch(incremento()));
+
+let botonDec = document.querySelector('#decremento')
+botonDec.addEventListener('click', () => store.dispatch(decremento()));
+
+let impar = document.querySelector('#incrementoImpar')
+impar.addEventListener('click', () => store.getState().contador %2!==0 && store.dispatch(incremento()));
+
+let async = document.querySelector('#incrementoAsync');
+async.addEventListener('click', () => {setTimeout( () => {
+  store.dispatch( incremento() ) }, 2000);
+})
+
+
+///////////////////
+// function hola() {
+//   console.log(valueImpar);
+// }
+// hola();
